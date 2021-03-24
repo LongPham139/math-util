85e77f569f95f91c71141b1e4cb3a1bcb81af4d4
LongPham139 <phamlong139@gmail.com>
2021-03-24 08:29:24 +0700

    Green 4EVER

--- src/com/longpt/mathutil/MathUtility.java
+++ src/com/longpt/mathutil/MathUtility.java
@@ -29,7 +29,7 @@ public class MathUtility {
         if(n == 0||n == 1)
             return 1; //khỏi mất công tính, return ngay
             //sống sót đến đoạn này n = 2..20
-        long result = 0;
+        long result = 1;
         for (int i = 2; i <= n; i++) {
             result *= i;
         }
